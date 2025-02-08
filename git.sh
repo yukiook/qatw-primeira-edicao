72e1e872bbab394ce3aa86c1f0fdbc13306b58dd
Papito <papitodev@proton.me>
2025-02-08 20:29:04 -0300

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
-git config --global user.name "Papito"
-git config --global user.email papitodev@proton.me
+git config --global user.name "Seu Nome"
+git config --global user.email seu-email-do@github.com
 
 ssh-keygen -t rsa -b 4096
